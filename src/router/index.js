79534f59dee549e702fbb55f21b24e3e3dfeee49
Oka233 <dit_shao@163.com>
2023-06-19 22:22:40 +0800

    remove unused files and packages

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,6 @@ export const constantRoutes = [
     component: () => import('@/views/404'),
     hidden: true
   },
-
   {
     path: '/',
     component: Layout,
@@ -53,28 +52,6 @@ export const constantRoutes = [
         name: 'StartWizard',
         component: () => import('@/views/startWizard/index'),
         meta: { title: 'StartWizard' }
-      }
-    ]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
       }
     ]
   },
